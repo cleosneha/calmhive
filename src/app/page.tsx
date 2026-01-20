1824c9c85d840c38b826f503a686792327b64dd0
Sneha Sharma <snehav2109@gmail.com>
2026-01-20 19:30:00 +0530

    feat: add HowItWorks, CTA, and FAQ components to enhance landing page functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import HeaderLoggedOut from "@/components/shared/header/header-logged-out";
 import Footer from "@/components/shared/footer/footer";
 import HeroSection from "@/components/landing-page/hero-section";
 import Purpose from "@/components/landing-page/purpose";
-import CTA from "@/components/landing-page/cta";
-import FAQ from "@/components/landing-page/faq";
+import CTA from "@/components/landing-page/cta-temp";
+import FAQ from "@/components/landing-page/faq-temp";
 import HowItWorks from "@/components/landing-page/how-it-works";
 
 export default function HomePage() {
